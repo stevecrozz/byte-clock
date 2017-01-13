0662fb61e5bbf1a4da2b8b94132bf7b9df862438
Stephen Crosby <stevecrozz@gmail.com>
2017-01-13 10:19:49 -0800

    blink an LED on IO pin 0
    https://www.youtube.com/watch?v=AjBlS9Ti0xE

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
-from datetime import date
+import usocket as socket
+import ustruct as struct
+import utime
+import machine
 
-try:
-    import usocket as socket
-    import utime as time
-    import ustruct as struct
-except:
-    import socket
-    import time
-    import struct
+from machine import RTC
+from machine import Pin
 
-# Seconds between 1900 and 1970
-TIME1900TO1970 = 2208988800L
+# (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
+NTP_DELTA = 3155673600
+host = "pool.ntp.org"
+global rtc_wake
+rtc_wake = False
 
-# Taken from:
-# https://github.com/micropython/micropython/blob/master/esp8266/scripts/ntptime.py
-def get_time(host):
+global pins;
+pins = {
+  0: Pin('GP0', mode=Pin.OUT),
+  1: Pin('GP1', mode=Pin.OUT)
+}
+
+def gettime():
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1b
     addr = socket.getaddrinfo(host, 123)[0][-1]
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(1)
-    res = s.sendto('\x1b' + 47 * '\0', addr)
+    res = s.sendto(NTP_QUERY, addr)
     msg = s.recv(48)
     s.close()
-    t = struct.unpack("!I", msg[40:44])[0]
-    t -= TIME1900TO1970
-    return time.ctime(t).replace("  "," ")
+    val = struct.unpack("!I", msg[40:44])[0]
+    return val - NTP_DELTA
 
-val = get_time('pool.ntp.org')
-print val
+def alarm_handler (rtc_o):
+    pins[0].toggle()
+    pins[1].toggle()
+
+t = gettime()
+tm = utime.localtime(t)
+rtc = RTC(datetime=tm)
+rtc.alarm(time=1000, repeat=True)
+rtc_i = rtc.irq(trigger=RTC.ALARM0, handler=alarm_handler, wake=machine.SLEEP | machine.IDLE)
