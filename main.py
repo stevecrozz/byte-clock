c4e606dac19244ee8c8980512c5c12706b7f4191
Stephen Crosby <stevecrozz@gmail.com>
2017-01-22 12:12:50 -0800

    refactor ntp clock components into a class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import usocket as socket
 import ustruct as struct
 import utime
 import machine
+import sys
 
 from machine import RTC
 from machine import Pin
@@ -31,18 +32,6 @@ for i in range(16):
 global wakeup
 wakeup = False
 
-def gettime():
-    NTP_QUERY = bytearray(48)
-    NTP_QUERY[0] = 0x1b
-    addr = socket.getaddrinfo(host, 123)[0][-1]
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.settimeout(1)
-    res = s.sendto(NTP_QUERY, addr)
-    msg = s.recv(48)
-    s.close()
-    val = struct.unpack("!I", msg[40:44])[0]
-    return val - NTP_DELTA
-
 def tick():
     global display_state
 
@@ -58,11 +47,31 @@ def alarm_handler (rtc_o):
     global wakeup
     wakeup = True
 
-t = gettime()
-tm = utime.localtime(t)
-rtc = RTC(datetime=tm)
-rtc.alarm(time=1000, repeat=True)
-rtc_i = rtc.irq(trigger=RTC.ALARM0, handler=alarm_handler, wake=machine.SLEEP | machine.IDLE)
+class TwentyFourHourClock:
+    def __init__(self):
+        time = self.fetch_ntp_time()
+        self.rtc = self.set_ntp_time(time)
+
+    def fetch_ntp_time(self):
+        NTP_QUERY = bytearray(48)
+        NTP_QUERY[0] = 0x1b
+        addr = socket.getaddrinfo(host, 123)[0][-1]
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(1)
+        res = s.sendto(NTP_QUERY, addr)
+        msg = s.recv(48)
+        s.close()
+        val = struct.unpack("!I", msg[40:44])[0]
+        return val - NTP_DELTA
+
+    def set_ntp_time(self, time):
+        tm = utime.localtime(time)
+        return RTC(datetime=tm)
+
+clock = TwentyFourHourClock()
+
+clock.rtc.alarm(time=1000, repeat=True)
+rtc_i = clock.rtc.irq(trigger=RTC.ALARM0, handler=alarm_handler, wake=machine.SLEEP | machine.IDLE)
 
 while True:
     machine.idle()
