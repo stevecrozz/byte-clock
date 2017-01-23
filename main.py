ac6a56fd81f263ed67d94198f28c6d663e4e5b5b
Stephen Crosby <stevecrozz@gmail.com>
2017-01-23 00:18:53 -0800

    add configurable offset and comments

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ import sys
 from machine import RTC
 from machine import Pin
 
+"""
+The onboard real-time clock (RTC) keeps track of the date and time the normal
+way. The clock can be synchronized with an NTP host and this avoids the problem
+of having to set the clock.
+"""
 class TwentyFourHourClock:
     # (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
     NTP_DELTA = 3155673600
+
+    def __init__(self, offset):
+        self.offset = offset * 60 * 60
 
     def sync_with_retries(self, host, tries):
         for i in range(0, tries):
@@ -33,7 +41,7 @@ class TwentyFourHourClock:
         msg = s.recv(48)
         s.close()
         val = struct.unpack("!I", msg[40:44])[0]
-        return val - self.NTP_DELTA
+        return val - self.NTP_DELTA + self.offset
 
     def set_ntp_time(self, time):
         tm = utime.localtime(time)
@@ -41,20 +49,29 @@ class TwentyFourHourClock:
 
     def seconds_since_midnight(self):
         now = self.rtc.now()
-        return now[3] * 24 * 60 + now[4] * 60 + now[5]
+        return now[3] * 60 * 60 + now[4] * 60 + now[5]
 
-class Counter:
-    LIGHT_COUNT = 8
-    MICROSECONDS_PER_DAY = 24 * 60 * 60 * 1000
-    PERIODS_PER_DAY = 2 ** LIGHT_COUNT
-    MICROSECONDS_PER_PERIOD = MICROSECONDS_PER_DAY // PERIODS_PER_DAY
-    MAX_DISPLAY_STATE = PERIODS_PER_DAY - 1
+"""
+A simple binary counter designed to count up to some maximum number, wrapping
+around before hitting a maximum value. The counter runs forever, incrementing
+its internal state periodically so that every day it counts through one full
+sequence. When the counter updates, it sends output to the provided display so
+the state can be visualized.
+"""
+class DayPartCounter:
+    SECONDS_PER_DAY = 24 * 60 * 60
+    MICROSECONDS_PER_DAY = SECONDS_PER_DAY * 1000
 
     def __init__(self, clock, display):
         self.tfhc = clock
         self.display = display
+
+        self.PERIODS_PER_DAY = 2 ** display.digits
+        self.MICROSECONDS_PER_PERIOD = self.MICROSECONDS_PER_DAY // self.PERIODS_PER_DAY
+        self.MAX_DISPLAY_STATE = self.PERIODS_PER_DAY - 1
+
         self.tick_ready = False
-        self.test()
+        self.set_interrupt()
         self.set_state()
 
     def handle_interrupt(self, rtc_o):
@@ -65,7 +82,8 @@ class Counter:
 
     def set_state(self):
         seconds_since_midnight = self.tfhc.seconds_since_midnight()
-        self.state = seconds_since_midnight // self.PERIODS_PER_DAY
+        microseconds_since_midnight = seconds_since_midnight * 1000
+        self.state = microseconds_since_midnight // self.MICROSECONDS_PER_PERIOD
         self.display.display(self.state)
 
     def test(self):
@@ -104,23 +122,27 @@ class Counter:
 
         self.display.display(self.state)
 
+"""
+A simple display that manages a series of output pins intended to drive one LED
+each.
+"""
 class Display:
-    PINS = {}
+    def __init__(self, digits):
+        self.digits = digits
+        self.display_formatter = '{0:0' + str(digits) + 'b}'
+        self.pins = {}
 
-    def __init__(self, pin_count):
-        self.DISPLAY_FORMATTER = '{0:0' + str(pin_count) + 'b}'
-
-        for i in range(pin_count):
-            self.PINS[i] = Pin('GP' + str(i), mode=Pin.OUT)
+        for i in range(digits):
+            self.pins[i] = Pin('GP' + str(i), mode=Pin.OUT)
 
     def display(self, state):
-        for i, v in enumerate(list(self.DISPLAY_FORMATTER.format(state))):
-            self.PINS[i].value(int(v))
+        for i, v in enumerate(list(self.display_formatter.format(state))):
+            self.pins[i].value(int(v))
 
-clock = TwentyFourHourClock()
+clock = TwentyFourHourClock(offset=-8)
 clock.sync_with_retries('pool.ntp.org', 3)
 
 display = Display(8)
 
-counter = Counter(clock, display)
+counter = DayPartCounter(clock, display)
 counter.run()
